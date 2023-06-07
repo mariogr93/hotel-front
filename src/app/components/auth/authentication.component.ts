705d6a0697a9719e3e21fc7d4c9e6e34206c34da
Mario Guadalupe <mariogr93@gmail.com>
2023-06-06 21:01:58 -0400

    create authenticantion component

---
+++ src/app/components/auth/authentication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:"authentication-component",
+    templateUrl:"./authentication.component.html",
+    styleUrls:["./authentication.component.html"]
+})
+export class AuthenticationComponent {
+    
+}
