c49581a091e1d16fc8fcf4c58cbef637b876a815
Mario Guadalupe <mariogr93@gmail.com>
2023-06-11 19:32:40 -0400

    container styles

--- src/app/components/auth/authentication.component.ts
+++ src/app/components/auth/authentication.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
 @Component({
     selector:"authentication-component",
     templateUrl:"./authentication.component.html",
-    styleUrls:["./authentication.component.html"]
+    styleUrls:["./authentication.component.css"]
 })
 export class AuthenticationComponent {
     tabSelected = true;
