491bdaef1e8069ad76e0ca705ea06ec7c8c9f894
Mario Guadalupe <mariogr93@gmail.com>
2023-06-10 22:51:31 -0400

    employee and client form

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output } from "@angular/core";
 import { AbstractControl, FormBuilder, FormControl, ValidationErrors, Validators } from "@angular/forms";
-import { ClientRegistration } from "src/app/models/user.interface";
+import { BehaviorSubject, Observable } from "rxjs";
+import { ClientRegistration, EmployeeRegistration, User } from "src/app/models/user.interface";
 
 @Component({
     selector:"register-component",
@@ -10,18 +11,21 @@ import { ClientRegistration } from "src/app/models/user.interface";
 
 export class RegisterComponent implements OnInit {
     
+
+    private isEmployeeForm$ = new BehaviorSubject<boolean>(false);
+    _isEmployeeForm: Observable<boolean> = this.isEmployeeForm$.asObservable()
+
     registerForm = this.fb.group({
         firstName: ['', [Validators.required]],
         lastName: ['', [Validators.required]],
         email: ['', [Validators.required, Validators.email]],        
-        role: ['', [Validators.required]],
-        gender: ['', [Validators.required]],
         phoneNumber: ['', [Validators.required]],
-        passportNumber: ['', [Validators.required]],
-        booking: ['', [Validators.required]],
         password: [ '', Validators.required ],
         confirmPassword: [ '', Validators.required ],
+        role: ['', [Validators.required]],
+        gender: ['', [Validators.required]],
     },  { validators: [ this.matchingPasswordsValidator ] })
+
 
 
     get firstNameControl(): FormControl {
@@ -56,6 +60,14 @@ export class RegisterComponent implements OnInit {
         return this.registerForm.get("booking") as FormControl
     }
 
+    get salaryControl(): FormControl {
+        return this.registerForm.get("salary") as FormControl
+    }
+
+    get hotelControl(): FormControl {
+        return this.registerForm.get("hotel") as FormControl
+    }
+
     get passwordControl(): FormControl {
         return this.registerForm.get("password") as FormControl;
     }
@@ -64,17 +76,42 @@ export class RegisterComponent implements OnInit {
         return this.registerForm.get("confirmPassword") as FormControl;
     }
 
+    get isAdminUser(): boolean {
+        return true;
+    }
+
     constructor(private fb: FormBuilder){}
 
     ngOnInit(): void {
-
+        this._isEmployeeForm.subscribe(val => val ? this.switchToEmployeeForm() : this.switchToClientForm())
     }
 
     onSubmit(){
         if (this.registerForm.valid) {
-            const newUser: ClientRegistration = this.getFrom()
+            if(this.isEmployeeForm$.value){
+                const newEmployee: EmployeeRegistration = this.getEmployeeForm()
+            } else {
+                const newClient: ClientRegistration = this.getClientForm()
+            }
         }
+    }
 
+    switchToEmployeeForm(){
+        this.registerForm.removeControl('passportNumber')
+        this.registerForm.removeControl('booking')
+        this.registerForm.addControl('salary', new FormControl('', Validators.required));
+        this.registerForm.addControl('hotel', new FormControl('', Validators.required));
+    }
+
+    switchToClientForm(){
+        this.registerForm.removeControl('salary')
+        this.registerForm.removeControl('hotel')
+        this.registerForm.addControl('passportNumber', new FormControl('', Validators.required));
+        this.registerForm.addControl('booking', new FormControl('', Validators.required));
+    }
+
+    toggleSwitch($e: any){
+        this.isEmployeeForm$.next($e.target.checked)
     }
 
     matchingPasswordsValidator(form: AbstractControl): ValidationErrors | null {
@@ -86,8 +123,8 @@ export class RegisterComponent implements OnInit {
             : { matchingPasswordsValidator: true };
     }
 
-    getFrom():ClientRegistration {
-        const signupForm: ClientRegistration = {
+    getGeneralForm():User {
+        let signupForm:User = { 
             firstName : this.firstNameControl.value,
             lastName: this.lastNameControl.value,
             email: this.emailControl.value,
@@ -95,9 +132,25 @@ export class RegisterComponent implements OnInit {
             role: this.roleControl.value,
             gender: this.genderControl.value,
             phoneNumber: this.phoneNumberControl.value,
-            passportNumber: this.passportNumberControl.value,
-            booking: this.bookingControl.value
         }
+
         return signupForm;
     }
+
+    getEmployeeForm():EmployeeRegistration {
+        const employeeForm: EmployeeRegistration = {...this.getGeneralForm(), 
+            salary: this.salaryControl.value, 
+            hotel: this.hotelControl.value
+        }
+        return employeeForm;
+    }
+    getClientForm():ClientRegistration {
+        const clientForm: ClientRegistration = {...this.getGeneralForm(), 
+            passportNumber: this.passportNumberControl.value, 
+            booking: this.bookingControl.value
+        }
+
+        return clientForm;
+    }
+
 }
